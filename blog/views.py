9ce544f0d3321eec0dd7f1ce03b452cd14ce54f3
Shoaib Iqbal Khan <shoaibiqbalkhan1@gmail.com>
2023-02-24 21:36:37 +0500

    creating posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -9,6 +10,9 @@ def home(request):
     } 
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About' })
