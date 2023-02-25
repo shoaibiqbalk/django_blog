c45641dabaffbca340ceba3b092abde7b984683a
shoaibiqbalk <sk721638@gmail.com>
2023-02-25 20:50:56 +0500

    Post Update added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (ListView, DetailView, 
+                                  CreateView, UpdateView
+                                  )
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
@@ -30,6 +32,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)  
 
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)  
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About' })
