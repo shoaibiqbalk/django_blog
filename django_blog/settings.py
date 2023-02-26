09ad51a11fe6a8b04c64614e08845ebaf14dc0d0
shoaibiqbalk <sk721638@gmail.com>
2023-02-26 22:59:14 +0500

    password reset email added

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -136,3 +136,8 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
