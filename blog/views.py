36ba3e6bb4d00f85657a88aac7ccffae6abe9738
shoaibiqbalk <sk721638@gmail.com>
2023-02-26 13:09:27 +0500

    posts and pagination added

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 3
 
 
 class PostDetailView(DetailView):
@@ -54,6 +55,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post 
     success_url = '/'
+    
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
